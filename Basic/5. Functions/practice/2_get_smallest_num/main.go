1eee8c6eb6456a7f1033451fafdd7132e8b73c71
Omar Tulashvili <omartulashvili.ge@gmail.com>
2025-12-03 23:29:49 +0300

    Functions Practice. Get smallest num. Create desicion in main func

--- Basic/5. Functions/practice/2_get_smallest_num/main.go
+++ Basic/5. Functions/practice/2_get_smallest_num/main.go
@@ -2,11 +2,30 @@ package main
 
 import "fmt"
 
+//	func getSmallestNum([]int) int {
+//		nums := []int{}
+//		for i := 0; i < len(nums); i++ {
+//			fmt.Println(i)
+//		}
+//
+// return i
+// }
 func main() {
-	nums := []int{4, 5, 6, 7}
+	nums := []int{4, 2, 1, 7}
 
-	for i := 0; i < len(nums); i++ {
-		fmt.Println(i)
+	val := nums[0] // 0
+	var iteration int
+	for _, v := range nums {
+		iteration++
+		fmt.Println("iteration: #", iteration)
+		fmt.Println("val in this iteration", val)
+		if val > v {
+			val = v
+		}
+
+		fmt.Println("num in this iteration", v)
+		fmt.Println("smallest value", val)
+		fmt.Println()
 	}
 
 }
