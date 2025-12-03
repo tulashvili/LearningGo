75738d0ab6a3cf91cfd9281d3dcd93eb437b1224
Omar Tulashvili <omartulashvili.ge@gmail.com>
2025-12-03 23:40:19 +0300

    Functions Practice. Get smallest num. Delete debug prints

--- Basic/5. Functions/practice/2_get_smallest_num/main.go
+++ Basic/5. Functions/practice/2_get_smallest_num/main.go
@@ -8,20 +8,15 @@ func getSmallestNum(nums [4]int) int {
 
 	for _, v := range nums {
 		iteration++
-		fmt.Println("iteration: #", iteration)
 
 		if minVal > v {
 			minVal = v
 		}
-
-		fmt.Println("num in this iteration", v)
-		fmt.Println("smallest minValue in this iteration", minVal)
-		fmt.Println()
 	}
 	return minVal
 }
 
 func main() {
 	nums := [4]int{4, 5, 6, 7}
-	fmt.Println("smallest minValue", getSmallestNum(nums))
+	fmt.Println(getSmallestNum(nums))
 }
