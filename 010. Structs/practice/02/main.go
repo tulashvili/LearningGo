5bc4d6d263ba1fafb70430af2ca7557e9f9018af
Omar Tulashvili <omartulashvili.ge@gmail.com>
2025-12-14 16:57:14 +0300

    Methods. Practice.

--- 010. Structs/practice/02/main.go
+++ 010. Structs/practice/02/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Server struct {
 	// Операционная система
@@ -20,15 +22,15 @@ type Server struct {
 	StorageValue int
 
 	// Какой сервис должен быть установлен
-	// Правило: nginx
-	ServiceInstall string
+	// Правило: nginx по-умолчанию, но можно добавлять
+	ServiceInstall []string
 
 	// Доступность сервера в/из интернет
 	// Правило: должен
 	InternetAccess bool
 }
 
-func (s *Server) ChangeOS(newOS string) {
+func (s Server) ChangeOS(newOS string) {
 	if newOS != "Ubuntu" {
 		fmt.Println("We need to use only Ubuntu")
 	}
@@ -45,19 +47,45 @@ func (s *Server) ChangeCpuNum(newCpuNumValue int) {
 	}
 }
 
+func (s *Server) ChangeRam(newRamValue int) {
+	if newRamValue < 4000 || newRamValue > 8000 {
+		fmt.Println("We need to use at least 4GM RAM and no more 8GB")
+		fmt.Println("The selected value is:", newRamValue)
+	} else {
+		s.RamValue = newRamValue
+		fmt.Println("I can increase RAM.")
+		fmt.Println("New value is:", newRamValue)
+	}
+}
+
+func (s *Server) AddInstallService(addService string) {
+	s.ServiceInstall = append(s.ServiceInstall, addService)
+}
+
 func main() {
 	server := Server{
 		OperationSystem: "Ubuntu",
 		CpuNum:          4,
-		RamValue:        3500,
+		RamValue:        4000,
 		StorageValue:    25000,
-		ServiceInstall:  "nginx",
+		ServiceInstall:  []string{"nginx"},
 		InternetAccess:  true,
 	}
 	fmt.Println("I need server with next chars:", server)
 	fmt.Println()
+
 	fmt.Println("Current CPU Num Cores:", server.CpuNum)
 	server.ChangeCpuNum(5)
+	fmt.Println()
+
 	server.ChangeOS("Debian")
-	
+	fmt.Println()
+
+	server.ChangeRam(5000)
+	fmt.Println()
+
+	ptr := &server
+	fmt.Println("Services on server before changed:", ptr.ServiceInstall)
+	server.AddInstallService("docker")
+	fmt.Println("Services on server after changed:", ptr.ServiceInstall)
 }
