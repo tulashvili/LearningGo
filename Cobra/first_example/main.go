977a28d490ef06ff95650321b23afe181b908425
Omar Tulashvili <omartulashvili.ge@gmail.com>
2026-01-17 18:24:51 +0300

    Work with Cobra. Theory

---
+++ Cobra/first_example/main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "cobra_first_example/cmd"
+
+func main() {
+	cmd.Execute()
+}
--- Database/first_example/internal/storage/insert.go
+++ Database/first_example/internal/storage/insert.go
@@ -5,16 +5,19 @@ import (
 	"test_work_db/internal/model"
 )
 
-func InsertRows(
-	conn *sql.DB,
-	e model.Entry,
-) error {
+func InsertRows(conn *sql.DB, e model.Entry) error {
 	query := `
 	INSERT INTO daily_log (day, category, question, scale)
 	VALUES ($1, $2, $3, $4)
 	;
 	`
-	_, err := conn.Exec(query, e.Day, e.Category, e.Question, e.Scale)
+	_, err := conn.Exec(
+		query,
+		e.Day,
+		e.Category,
+		e.Question,
+		e.Scale,
+	)
 
 	return err
 }
